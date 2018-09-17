32a45e1605bdec5b906ff728088d72b31fd025a0
earthlingJen <jk.winkler@rocketmail.com>
2018-09-17 14:03:38 +0200

    Add header, first content(tiles and grid) and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const StyledHeader = styled.h1`
   background: #50e379;
   color: white;
-  padding: 12px;
+  padding: 20px 10px 10px;
   margin: -10px;
   display: flex;
   justify-content: center;
