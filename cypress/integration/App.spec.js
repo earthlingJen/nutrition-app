e4e12e53268b6610e5a5cb2b9b5b8ae2be1eca12
earthlingJen <jk.winkler@rocketmail.com>
2018-09-19 18:49:58 +0200

    Add unit and cypress integration testing

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -6,12 +6,29 @@ describe('App', () => {
     cy.clearLocalStorage()
   })
 
-  it('shows header and text', () => {
+  xit('shows header and text', () => {
     cy.get('div').should('exist')
     cy.get('h1').should('exist')
 
     cy.get('div')
       .should('contain', '25 bis unter 51')
       .should('exist')
+  })
+
+  it('shows nutriScreen ', () => {
+    cy.get('select')
+      .first()
+      .should('exist')
+      .select('männlich')
+    cy.get('div')
+      .should('contain', 'Magnesium')
+      .should('contain', '350')
+
+    cy.get('select')
+      .first()
+      .select('weiblich')
+    cy.get('div')
+      .should('contain', 'Magnesium')
+      .should('contain', '300')
   })
 })
--- src/reducer.spec.js
+++ src/reducer.spec.js
@@ -1,4 +1,5 @@
 import reducer from './reducer'
+import ACTIONS from './actions'
 
 describe('reducer', () => {
   it('returns the original state again', () => {
@@ -6,5 +7,51 @@ describe('reducer', () => {
     const someAction = { type: 'something unknown' }
 
     expect(reducer(state, someAction)).toBe(state)
+  })
+
+  describe(ACTIONS.SET_SELECT_SEX, () => {
+    it('changes the sex', () => {
+      const state = {
+        user: {
+          sex: 'female',
+          age: '25to50',
+        },
+      }
+
+      const action = {
+        type: ACTIONS.SET_SELECT_SEX,
+        payload: 'male',
+      }
+
+      expect(reducer(state, action)).toEqual({
+        user: {
+          sex: 'male',
+          age: '25to50',
+        },
+      })
+    })
+  })
+
+  describe(ACTIONS.SET_SELECT_AGE, () => {
+    it('changes the age', () => {
+      const state = {
+        user: {
+          sex: 'female',
+          age: '25to50',
+        },
+      }
+
+      const action = {
+        type: ACTIONS.SET_SELECT_AGE,
+        payload: '15to18',
+      }
+
+      expect(reducer(state, action)).toEqual({
+        user: {
+          sex: 'female',
+          age: '15to18',
+        },
+      })
+    })
   })
 })
