5ca88c7740105c4ad8beb61853454b99ce1364c7
earthlingJen <jk.winkler@rocketmail.com>
2018-09-25 17:46:30 +0200

    Add stories for foodTile, knobs for nutritionTile

--- src/stories/storybook.js
+++ src/stories/storybook.js
@@ -2,11 +2,11 @@
 
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs/react'
-import NutritionTile from '../components/NutritionTile'
+import { withKnobs, text, number } from '@storybook/addon-knobs/react'
 import AgeSelect from '../components/AgeSelect'
+import FoodTile from '../components/FoodTile'
+import NutritionTile from '../components/NutritionTile'
 import SexSelect from '../components/SexSelect'
-
 import { injectGlobal } from 'styled-components'
 
 injectGlobal`
@@ -21,17 +21,26 @@ injectGlobal`
     
   }
 `
+storiesOf('AgeSelect', module).add('with text', () => <AgeSelect />)
+
+storiesOf('FoodTile', module)
+  .addDecorator(withKnobs)
+  .add('with text', () => (
+    <FoodTile
+      veggieName={text('Veggie', 'Spinat')}
+      veggieIcon="icon"
+      veggieValue={number('VeggieValue: ', 300) + text('unit', ' mg')}
+    />
+  ))
 
 storiesOf('NutritionTile', module)
   .addDecorator(withKnobs)
   .add('with text', () => (
-    <NutritionTile nutriName="Magnesium" nutriValue="300" nutriUnit="mg" />
+    <NutritionTile
+      nutriName={text('NutritionName: ', 'Magnesium')}
+      nutriValue={number('NutritionValue: ', 300)}
+      nutriUnit={text('NutritionUnit: ', 'mg')}
+    />
   ))
 
-storiesOf('AgeSelect', module)
-  .addDecorator(withKnobs)
-  .add('with text', () => <AgeSelect />)
-
-storiesOf('SexSelect', module)
-  .addDecorator(withKnobs)
-  .add('with text', () => <SexSelect />)
+storiesOf('SexSelect', module).add('with text', () => <SexSelect />)
