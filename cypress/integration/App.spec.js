80397f12aad588b1c2823d2533cc7d3195cd0ead
earthlingJen <jk.winkler@rocketmail.com>
2018-09-25 18:39:58 +0200

    Add cypress test for ageSelect and linking to planningScreen

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -6,7 +6,7 @@ describe('App', () => {
     cy.clearLocalStorage()
   })
 
-  xit('shows header and text', () => {
+  it('shows header and text', () => {
     cy.get('div').should('exist')
     cy.get('h1').should('exist')
 
@@ -30,5 +30,22 @@ describe('App', () => {
     cy.get('div')
       .should('contain', 'Magnesium')
       .should('contain', '300')
+  })
+
+  it('displays correct value', () => {
+    cy.get('select')
+      .last()
+      .should('exist')
+      .select('15to18')
+    cy.get('div')
+      .should('contain', 'Kalzium')
+      .should('contain', '1200')
+  })
+
+  it('leads to planning screen', () => {
+    cy.get('div')
+      .contains('Eisen')
+      .click()
+      .request('http://localhost:3000/planning')
   })
 })
